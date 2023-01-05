0d08d849f51b88f16727fadf4d654dc2e9b66e3e
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-01-05 15:04:58 -0300

    Add view veterinario

--- venv/src/view/mostarar.py
+++ venv/src/view/mostarar.py
@@ -4,6 +4,7 @@ from src.model.agendamentoConsulta import AgentamentoConsulta
 from src.model.funcionario import Funcionario
 from src.model.prontuario import Prontuario
 from src.model.vacina import Vacina
+from src.model.veterinario import Veterinario
 
 from src.controller.animalController import AnimalController
 from src.controller.clienteController import ClienteController
@@ -11,6 +12,7 @@ from src.controller.consultaController import ConsultaController
 from src.controller.funcionarioController import FuncionarioController
 from src.controller.prontuarioController import ProntuarioController
 from src.controller.vacinaController import VacinaController
+from src.controller.veterinarioController import VeterinarioController
 
 animal = Animal(1, 'Piroquinhas', 'Felino', 'M', 'Cianes', 1.2, '20/08/2020')
 cliente = Cliente(2, 'Guilherme', '07341965398', '28/01/1999',
@@ -23,9 +25,12 @@ prontuario = Prontuario(1, 'M', 'Porte', 'Especie', '22/01/2020', 'Cianes')
 
 vacina = Vacina(1, 'VaXina', 'Xina', '28/08/2023', '30L')
 
+veterinario = Veterinario(1, 'Dr. Rubens', '09876543287', '12/12/1212', 'rubens@gmail.com', 'sistemas de informação')
+
 clienteControle = ClienteController.controllerCliente(cliente)
 animalControle = AnimalController.controllerAnimal(animal)
 agendamentoControle = ConsultaController.controllerConsulta(agendamento)
 funcionarioControle = FuncionarioController.controllerFuncionario(funcionario)
 prontuarioControle = ProntuarioController.controllerProntuario(prontuario)
 vacinaControle = VacinaController.controllerVacina(vacina)
+VeterinarioController = VeterinarioController.controllerVeterinario(veterinario)
