14f5b6459774b4fc82d9f217f643a37af0edb057
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-01-07 22:29:12 -0300

    Somente o cliente para testes

--- venv/src/view/mostarar.py
+++ venv/src/view/mostarar.py
@@ -16,28 +16,4 @@ from src.controller.veterinarioController import VeterinarioController
 
 cliente = Cliente(2, 'Guilherme', '07341965398', '28/01/1999',
                   'guilherme.gb@hotmail.com', '63400000', 'Centro', 'Av. Pedro Lopes', '88996431916')
-animal = Animal(3, 'nomeAnimal', 'especie', 'sexo',
-                'raca', 12.2, 'nascimento',  cliente)
-
-funcionario = Funcionario(1, 'Nome Funcionario', '20/06/2018',
-                          'email@funcionario.com', '9988776676', 'Funcionario')
-agendamento = AgentamentoConsulta(
-    10, 'data', 'horario', 120.80, 'observacao', animal, funcionario)
-vacina = Vacina(1, 'VaXina', 'Xina', '28/08/2023', '30L')
-
-veterinario = Veterinario(1, 'Dr. Rubens', '09876543287',
-                          '12/12/1212', '889982328923', 'rubens@gmail.com',
-                          'sistemas de informação')
-
-prontuario = Prontuario(23, 'sexo', 'porte', 'especie',
-                        'dataProntuario', 'racaProntuario', animal,
-                        veterinario, vacina)
-
 clienteControle = ClienteController.controllerCliente(cliente)
-animalControle = AnimalController.controllerAnimal(animal)
-agendamentoControle = ConsultaController.controllerConsulta(agendamento)
-funcionarioControle = FuncionarioController.controllerFuncionario(funcionario)
-vacinaControle = VacinaController.controllerVacina(vacina)
-pontuarioControle = ProntuarioController.controllerProntuario(prontuario)
-VeterinarioController = VeterinarioController.controllerVeterinario(
-    veterinario)
