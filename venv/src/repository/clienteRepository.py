75c8237385a62be6699e41d825d0eedfb6cc5afb
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-01-08 10:17:00 -0300

    Insert na tabela cliente do banco de dados

--- venv/src/repository/clienteRepository.py
+++ venv/src/repository/clienteRepository.py
@@ -2,13 +2,14 @@ from src.config.database import Conexao
 
 
 class ClientRepository:
-    def repositoryCliente(id, nomeCliente: str, cpf, nascimentoCliente, email, endereco, bairro, rua, telefoneCliente):
+    def repositoryCliente(id, nomeCliente, cpf, nascimentoCliente,
+                          telefoneCliente, email, rua, bairro, n_rua):
 
         con = Conexao.getConnection('')
         cursor = con.cursor()
 
         sql = "insert into cliente (nome, cpf, nascimento, telefone, email, rua, bairro, numero) values(%s, %s, %s, %s, %s, %s, %s, %s);"
-        value = (nomeCliente, cpf, nascimentoCliente, email,
-                 endereco, bairro, rua, telefoneCliente)
+        value = (nomeCliente, cpf, nascimentoCliente, telefoneCliente, email,
+                 rua, bairro, n_rua)
         cursor.execute(sql, value)
         con.commit()
