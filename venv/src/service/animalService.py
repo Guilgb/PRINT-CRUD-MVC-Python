40044790a04daca2efc1489084453dfd9a935539
Guilherme Bezerra <88347940+Guilgb@users.noreply.github.com>
2023-02-17 11:25:29 -0300

    correcao models animal

--- venv/src/service/animalService.py
+++ venv/src/service/animalService.py
@@ -30,6 +30,6 @@ class AnimalService:
         raca = str(animal.raca)
         peso = float(animal.peso)
         nascimento = str(animal.nascimento)
-        cliente = str(animal.cliente.nomeCliente)
+        cliente = str(animal.cliente)
         AnimalRepository.updateRepositoryAnimal(
             id, nomeAnimal, especie, sexo, raca, peso, nascimento, cliente)
